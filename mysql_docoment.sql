01aac34de0172c4f222db1c4a13262bd89e92fcf
zhangyuanb@home <luoyiguren@outlook.com>
2018-11-19 19:48:01 +0800

    mysql build-in-function

--- mysql_docoment.sql
+++ mysql_docoment.sql
@@ -318,6 +318,114 @@
 -- YEAR()	Return the year
 -- YEARWEEK()	Return the year and week
 
+SELECT 1 + '1';
+SELECT CONCAT(2,' test');
+SELECT 38.5, CAST(38.5 AS CHAR);
+SELECT NULL<=>NULL;   -- 1 <=> null-safe equality comparison operator.
+
+SELECT 1>'6x';
+SELECT 7>'6x';
+SELECT 0>'x6'
+SELECT 0='x6';
+
+SELECT * FROM tbl_name WHERE str_col=1; -- if str_col has index,now index can not work.
+SELECT '18015376320243458' = 18015376320243458;
+SELECT '18015376320243459' = 18015376320243459;
+SELECT '18015376320243459'+0.0;
+
+-- 12.5 String Functions
+-- Name	Description
+-- ASCII()	Return numeric value of left-most character
+-- BIN()	Return a string containing binary representation of a number
+-- BIT_LENGTH()	Return length of argument in bits
+-- CHAR()	Return the character for each integer passed
+-- CHAR_LENGTH()	Return number of characters in argument
+-- CHARACTER_LENGTH()	Synonym for CHAR_LENGTH()
+-- CONCAT()	Return concatenated string
+-- CONCAT_WS()	Return concatenate with separator
+-- ELT()	Return string at index number
+-- EXPORT_SET()	Return a string such that for every bit set in the value bits, you get an on string and for every unset bit, you get an off string
+-- FIELD()	Return the index (position) of the first argument in the subsequent arguments
+-- FIND_IN_SET()	Return the index position of the first argument within the second argument
+-- FORMAT()	Return a number formatted to specified number of decimal places
+-- HEX()	Return a hexadecimal representation of a decimal or string value
+-- INSERT()	Insert a substring at the specified position up to the specified number of characters
+-- INSTR()	Return the index of the first occurrence of substring
+-- LCASE()	Synonym for LOWER()
+-- LEFT()	Return the leftmost number of characters as specified
+-- LENGTH()	Return the length of a string in bytes
+-- LIKE	Simple pattern matching
+-- LOAD_FILE()	Load the named file
+-- LOCATE()	Return the position of the first occurrence of substring
+-- LOWER()	Return the argument in lowercase
+-- LPAD()	Return the string argument, left-padded with the specified string
+-- LTRIM()	Remove leading spaces
+-- MAKE_SET()	Return a set of comma-separated strings that have the corresponding bit in bits set
+-- MATCH	Perform full-text search
+-- MID()	Return a substring starting from the specified position
+-- NOT LIKE	Negation of simple pattern matching
+-- NOT REGEXP	Negation of REGEXP
+-- OCT()	Return a string containing octal representation of a number
+-- OCTET_LENGTH()	Synonym for LENGTH()
+-- ORD()	Return character code for leftmost character of the argument
+-- POSITION()	Synonym for LOCATE()
+-- QUOTE()	Escape the argument for use in an SQL statement
+-- REGEXP	Whether string matches regular expression
+-- REPEAT()	Repeat a string the specified number of times
+-- REPLACE()	Replace occurrences of a specified string
+-- REVERSE()	Reverse the characters in a string
+-- RIGHT()	Return the specified rightmost number of characters
+-- RLIKE	Whether string matches regular expression
+-- RPAD()	Append string the specified number of times
+-- RTRIM()	Remove trailing spaces
+-- SOUNDEX()	Return a soundex string
+-- SOUNDS LIKE	Compare sounds
+-- SPACE()	Return a string of the specified number of spaces
+-- STRCMP()	Compare two strings
+-- SUBSTR()	Return the substring as specified
+-- SUBSTRING()	Return the substring as specified
+-- SUBSTRING_INDEX()	Return a substring from a string before the specified number of occurrences of the delimiter
+-- TRIM()	Remove leading and trailing spaces
+-- UCASE()	Synonym for UPPER()
+-- UNHEX()	Return a string containing hex representation of a number
+-- UPPER()	Convert to uppercase
+
+-- For functions that operate on string positions,the first position is numbered.
+SELECT ASCII('2');
+SELECT ASCII(2);
+SELECT ASCII('dx');
+
+SELECT BIT_LENGTH('test');
+SELECT BIT_LENGTH('张');
+
+SELECT CHAR(77,121,83,81,'76');
+SELECT CHAR(77,77,77.3);
+
+SELECT HEX(CHAR(1,0)),HEX(CHAR(256));
+SELECT HEX(CHAR(1,0,0)),HEX(CHAR(256*256));
+
+SELECT CHARSET(CHAR(X'65')),CHARSET(CHAR(X'65' USING utf8));
+
+SELECT CHAR_LENGTH('张渊博'),LENGTH('张渊博') -- result 3 ,9 
+SELECT CHAR_LENGTH('zyb'),LENGTH('zyb');	-- result：3 3 	
+-- LENGTH return the length string in bytes
+-- CHAR_LENGTH Return number of characters in argument
+-- CHARACTER_LENGTH is a synonym for CHAR_LENGTH(同义词)
+
+-- CONCAT() returns null if any argument is null
+SELECT CONCAT('My','s','QL'),CONCAT('My',NULL,'QL'),CONCAT(14.3);  -- result MysQL null 14.3
+
+SELECT CONCAT_WS(',','First name','Second name','Last Name');
+SELECT CONCAT_WS(',','First name',NULL,'Last Name');	-- CONCAT_WS does not ship empty
+-- strings.However,it does ship any NULL values after the separator argument.
+
+SELECT ELT(1,'Aa','Bb','Cc','Dd');
+SELECT ELT(4,'Aa','Bb','Cc','Dd');
+
+SELECT FIELD('Bb', 'Aa', 'Bb', 'Cc', 'Dd', 'Ff');
+SELECT FIELD('Gg', 'Aa', 'Bb', 'Cc', 'Dd', 'Ff');
+
+SELECT FIND_IN_SET('b','a,b,c,d');
 
 
 -- 13.1.13 CREATE INDEX Syntax
